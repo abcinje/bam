71d69f587084cb77e921660965dfd8f2dacf0c7f
Injae Kang <abcinje@gmail.com>
2025-03-07 09:31:17 +0000

    Allow page sizes that are multiples of 4096 bytes

--- benchmarks/file/main.cu
+++ benchmarks/file/main.cu
@@ -149,8 +149,8 @@ int main(int argc, char** argv)
             std::cerr << "Number of requests must be 1\n";
             exit(1);
         }
-        if (page_size != 4096 && page_size != 8192) {
-            std::cerr << "Page size must be 4096 or 8192\n";
+        if (page_size & 0xfff) {
+            std::cerr << "Page size must be a multiple of 4096\n";
             exit(1);
         }
 
